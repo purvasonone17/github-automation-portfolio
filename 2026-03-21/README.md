e8ad61a497cd4210fbeecf392d99e9d628a61071
Purva Sonone <purvasonone@gmail.com>
2026-03-21 21:15:20 +0530

    Automated Update: 2026-03-21

---
+++ 2026-03-21/README.md
@@ -0,0 +1,8 @@
+# Express API Basics
+
+Learn how to build a basic REST API using Express.js.
+
+## Tech Stack
+- Node.js
+- GitHub API
+- MongoDB
---
+++ 2026-03-21/code.js
@@ -0,0 +1,13 @@
+// Topic: Express API Basics
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from your auto-generated API!');
+});
+
+app.listen(PORT, () => {
+  console.log('Server running on http://localhost:' + PORT);
+});
